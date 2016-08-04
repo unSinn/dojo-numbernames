fdb6a6ffdc1028d7df3d0a2d1cdadd7873538de4
Fabian Mächler <fabian.maechler@noser.com>
2016-08-04 18:12:12 +0200

    OneToThirty

---
+++ src/com/noser/NumberNamer.java
@@ -0,0 +1,105 @@
+package com.noser;
+
+import java.util.ArrayList;
+
+/**
+ * Created by fabian on 8/4/16.
+ */
+public class NumberNamer {
+
+    String printNumberName(int z) {
+        ArrayList<String> words = new ArrayList<>();
+
+        if (z == 0) {
+            words.add("zero");
+        } else {
+            if (z <= 20) {
+                words.add(getBelowTwenty(z));
+            } else {
+                words.add(getZehner(z));
+                words.add(getEiner(z));
+            }
+        }
+
+        words.removeIf(s -> s.equals(""));
+        return String.join(" ", words);
+    }
+
+    private String getBelowTwenty(int z) {
+        switch (z) {
+            case 0:
+                return "";
+            case 1:
+                return "one";
+            case 2:
+                return "two";
+            case 3:
+                return "three";
+            case 4:
+                return "four";
+            case 5:
+                return "five";
+            case 6:
+                return "six";
+            case 7:
+                return "seven";
+            case 8:
+                return "eight";
+            case 9:
+                return "nine";
+            case 10:
+                return "ten";
+            case 11:
+                return "eleven";
+            case 12:
+                return "twelve";
+            case 13:
+                return "thirteen";
+            case 14:
+                return "fourteen";
+            case 15:
+                return "fifteen";
+            case 16:
+                return "sixteen";
+            case 17:
+                return "seventeen";
+            case 18:
+                return "eighteen";
+            case 19:
+                return "nineteen";
+            case 20:
+                return "twenty";
+        }
+        return "ERROR BELOWTWENTY";
+    }
+
+    public String getEiner(int z) {
+        int einer = z % 10;
+        return getBelowTwenty(einer);
+    }
+
+    public String getZehner(int z) {
+        int zehner = (z / 10) % 10;
+        switch (zehner) {
+            case 0:
+                return "";
+            case 2:
+                return "twenty";
+            case 3:
+                return "thirty";
+            case 4:
+                return "fourty";
+            case 5:
+                return "fifty";
+            case 6:
+                return "sixty";
+            case 7:
+                return "seventy";
+            case 8:
+                return "eighty";
+            case 9:
+                return "ninety";
+        }
+        return "ERROR ZEHNER";
+    }
+}
---
+++ test/com/noser/NumberNameTests.java
@@ -0,0 +1,78 @@
+package com.noser;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by fabian on 8/4/16.
+ */
+public class NumberNameTests {
+
+    NumberNamer namer = new NumberNamer();
+
+    @Test
+    public void zero() {
+        String s = namer.printNumberName(0);
+        assertThat(s, is("zero"));
+    }
+
+    @Test
+    public void one() {
+        String s = namer.printNumberName(1);
+        assertThat(s, is("one"));
+    }
+
+    @Test
+    public void two() {
+        String s = namer.printNumberName(2);
+        assertThat(s, is("two"));
+    }
+
+    @Test
+    public void ten() {
+        String s = namer.printNumberName(10);
+        assertThat(s, is("ten"));
+    }
+
+    @Test
+    public void eleven() {
+        String s = namer.printNumberName(11);
+        assertThat(s, is("eleven"));
+    }
+
+    @Test
+    public void twenty() {
+        String s = namer.printNumberName(20);
+        assertThat(s, is("twenty"));
+    }
+
+    @Test
+    public void twentyone() {
+        String s = namer.printNumberName(21);
+        assertThat(s, is("twenty one"));
+    }
+
+    @Test
+
+    public void twentyfive() {
+        String s = namer.printNumberName(25);
+        assertThat(s, is("twenty five"));
+    }
+
+    @Test
+    public void twentynine() {
+        String s = namer.printNumberName(29);
+        assertThat(s, is("twenty nine"));
+    }
+
+    @Test
+    public void thirty() {
+        String s = namer.printNumberName(30);
+        assertThat(s, is("thirty"));
+    }
+
+}
